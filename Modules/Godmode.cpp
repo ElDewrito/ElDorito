f4c5e581f37898fec09a3805b567e0ffffc31c1a
Wunkolo <wunkolo@gmail.com>
2015-03-28 16:43:08 -0700

    Godemode cleanup

--- Modules/Godmode.cpp
+++ Modules/Godmode.cpp
@@ -33,23 +33,26 @@ void Godmode::Tick(const std::chrono::duration<double>& Delta)
 
 void Godmode::Run(const std::vector<std::string>& Args)
 {
-	if (Args.size() >= 2)
-	{
+	const size_t Offset = 0x75636A;
+	const uint8_t reset[8] = { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
+	const uint8_t god[8] = { 0xF3, 0x0F, 0x11, 0x97, 0x00, 0x01, 0x00, 0x00 };
 
-		if (Args[1] == "disable") {
+	if( Args.size() >= 2 )
+	{
+		if( Args[1].compare("off") == 0 )
+		{
 			// Disable it.
 			std::cout << "Disabling god map" << std::endl;
 			enabled = false;
-			memcpy((uint8_t*)GetModuleBase() + 0x075636A, reset, 8);
-
+			memcpy(((uint8_t*)GetModuleBase()) + Offset, reset, sizeof(reset));
 		}
-		else if (Args[1] == "enable") {
+		else if( Args[1].compare("on") == 0 )
+		{
 			// Enable
 			std::cout << "Enabling god map" << std::endl;
 			enabled = true;
-			memcpy((uint8_t*)GetModuleBase() + 0x075636A, god, 8);
+			memcpy(((uint8_t*)GetModuleBase()) + Offset, god, sizeof(god));
 		}
-
 	}
 	return;
 }
--- Modules/Godmode.h
+++ Modules/Godmode.h
@@ -9,12 +9,9 @@ public:
 	Godmode();
 	~Godmode();
 
-	bool enabled; 
-	uint8_t reset[8];
-	uint8_t god[8];
-
 	std::string Info();
 	void Tick(const std::chrono::duration<double>& Delta);
 	void Run(const std::vector<std::string>& Args);
 private:
+	bool enabled;
 };
