203cdec34475ca262f5c029568e5c59e5c6ccf65
emoose <abc@cock.li>
2015-04-03 03:53:17 +0100

    add WIP gamevariant struct

--- BlamTypes.h
+++ BlamTypes.h
@@ -52,6 +52,83 @@ namespace Blam
 		"multiplayer",
 		"shared",
 	};
+
+	// todo: safely make this into a C++ struct without any padding/packing etc
+	typedef struct _BLAM_GAME_VARIANT
+	{
+		/* 0xB2C */ uint32_t GameType; // see Blam::GameType
+		float Unknown1; // gravity maybe?
+		uint8_t Unknown2[0x2C];
+		/* 0xB60 */ uint8_t Unknown3[0xF0]; // start of variant data
+		/* 0xC50 */ uint8_t Unknown4;
+		uint8_t RoundTimeLimit; // default 8
+		uint8_t NumberOfRounds; // default 1 (this might be wrong)
+		uint8_t Unknown5; // default 2
+		uint8_t Unknown6; // default 0x10
+		uint8_t Unknown7; // 0
+		uint8_t Unknown8; // 0
+		uint8_t Unknown9; // default 5
+		/* 0xC58 */ uint8_t Unknown10; // default 0xA
+		uint8_t RespawnTime; // default 5 (gets doubled by russian coders who can't into game variant data)
+		// also this is the actual time till player gets respawned without pressing buttons, the respawn time shown is sometimes a few seconds sooner for some reason
+
+		uint8_t Unknown11; // default 5
+		uint8_t Unknown12; // 0
+		uint8_t Unknown13; // default 5 
+		uint8_t Unknown14; // 0
+		uint8_t Unknown15; // 0
+		uint8_t Unknown16; // 0
+		/* 0xC60 */ uint8_t Unknown17; // 0
+		uint8_t Unknown18; // 0
+		uint8_t Unknown19; // 0
+		uint8_t Unknown20; // 0
+		uint8_t Unknown21; // 0
+		uint8_t Unknown22; // 0
+		uint8_t Unknown23; // 0
+		uint8_t Unknown24; // 0
+		/* 0xC68 */ uint8_t Unknown25; // 0
+		uint8_t Unknown26; // 0
+		uint8_t Unknown27; // 0
+		uint8_t Unknown28; // default 0xFE
+		uint8_t Unknown29; // default 0xFE
+		uint8_t Unknown30; // 0
+		uint8_t Unknown31; // 0
+		uint8_t Unknown32; // 0
+		/* 0xC70 */ uint8_t Unknown33; // 0
+		uint8_t Unknown34; // 0
+		uint8_t Unknown35; // 0
+		uint8_t Unknown36; // 0
+		uint8_t Unknown37; // 0
+		uint8_t Unknown38; // 0
+		uint8_t Unknown39; // 0
+		uint8_t Unknown40; // 0
+		/* 0xC78 */ uint8_t Unknown41; // 0
+		uint8_t Unknown42; // 0
+		uint8_t Unknown43; // 0
+		uint8_t Unknown44; // 0
+		uint8_t Unknown45; // 0
+		uint8_t Unknown46; // default 7
+		uint8_t Unknown47; // 0
+		uint8_t Unknown48; // default 1
+		/* 0xC80 */ uint8_t Unknown49; // 0
+		uint8_t Unknown50; // default 1
+		uint8_t Unknown51; // 0
+		uint8_t Unknown52; // 0
+		uint8_t Unknown53; // 0
+		uint8_t Unknown54; // 0
+		uint8_t Unknown55; // 0
+		uint8_t Unknown56; // 0
+		/* 0xC88 */ uint8_t Unknown57; // 0
+		uint8_t Unknown58; // 0
+		uint8_t Unknown59; // 0
+		uint8_t Unknown60; // 0
+		uint8_t Unknown61; // 0
+		uint8_t Unknown62; // 0
+		uint8_t Unknown63; // 0
+		uint8_t Unknown64; // default 0xFE
+		uint8_t Unknown65; // default 0xFE
+		// theres a lot more bytes too
+	} BLAM_GAME_VARIANT, *PBLAM_GAME_VARIANT;
 }
 
 enum class CameraType : uint8_t
