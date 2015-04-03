b78fba4ffd5829b95850b40b825af5b7c6ebcae6
Jake Shirley <navi@odinseye.org>
2015-04-02 22:20:41 -0700

    Added top level exception handler to print out exceptions

--- main.cpp
+++ main.cpp
@@ -13,17 +13,96 @@
 HINSTANCE g_hThisInstance = 0;
 HMODULE g_hOriginalDll = 0;
 
+LONG WINAPI TopLevelExceptionHandler(unsigned int code, EXCEPTION_POINTERS *pExceptionInfo)
+{
+	const char *error_type = "Unknown";
+	switch(code)
+	{
+	case EXCEPTION_ACCESS_VIOLATION:
+		error_type = "EXCEPTION_ACCESS_VIOLATION";
+		break;
+	case EXCEPTION_ARRAY_BOUNDS_EXCEEDED:
+		error_type = "EXCEPTION_ARRAY_BOUNDS_EXCEEDED";
+		break;
+	case EXCEPTION_BREAKPOINT:
+		error_type = "EXCEPTION_BREAKPOINT";
+		break;
+	case EXCEPTION_DATATYPE_MISALIGNMENT:
+		error_type = "EXCEPTION_DATATYPE_MISALIGNMENT";
+		break;
+	case EXCEPTION_FLT_DENORMAL_OPERAND:
+		error_type = "EXCEPTION_FLT_DENORMAL_OPERAND";
+		break;
+	case EXCEPTION_FLT_DIVIDE_BY_ZERO:
+		error_type = "EXCEPTION_FLT_DIVIDE_BY_ZERO";
+		break;
+	case EXCEPTION_FLT_INEXACT_RESULT:
+		error_type = "EXCEPTION_FLT_INEXACT_RESULT";
+		break;
+	case EXCEPTION_FLT_INVALID_OPERATION:
+		error_type = "EXCEPTION_FLT_INVALID_OPERATION";
+		break;
+	case EXCEPTION_FLT_OVERFLOW:
+		error_type = "EXCEPTION_FLT_OVERFLOW";
+		break;
+	case EXCEPTION_FLT_STACK_CHECK:
+		error_type = "EXCEPTION_FLT_STACK_CHECK";
+		break;
+	case EXCEPTION_FLT_UNDERFLOW:
+		error_type = "EXCEPTION_FLT_UNDERFLOW";
+		break;
+	case EXCEPTION_ILLEGAL_INSTRUCTION:
+		error_type = "EXCEPTION_ILLEGAL_INSTRUCTION";
+		break;
+	case EXCEPTION_IN_PAGE_ERROR:
+		error_type = "EXCEPTION_IN_PAGE_ERROR";
+		break;
+	case EXCEPTION_INT_DIVIDE_BY_ZERO:
+		error_type = "EXCEPTION_INT_DIVIDE_BY_ZERO";
+		break;
+	case EXCEPTION_INT_OVERFLOW:
+		error_type = "EXCEPTION_INT_OVERFLOW";
+		break;
+	case EXCEPTION_INVALID_DISPOSITION:
+		error_type = "EXCEPTION_INVALID_DISPOSITION";
+		break;
+	case EXCEPTION_NONCONTINUABLE_EXCEPTION:
+		error_type = "EXCEPTION_NONCONTINUABLE_EXCEPTION";
+		break;
+	case EXCEPTION_PRIV_INSTRUCTION:
+		error_type = "EXCEPTION_PRIV_INSTRUCTION";
+		break;
+	case EXCEPTION_SINGLE_STEP:
+		error_type = "EXCEPTION_SINGLE_STEP";
+		break;
+	case EXCEPTION_STACK_OVERFLOW:
+		error_type = "EXCEPTION_STACK_OVERFLOW";
+		break;
+	}
+
+	// Format exception string
+	std::cout << "Exception: 0x" << std::uppercase << std::hex << pExceptionInfo->ExceptionRecord->ExceptionCode << std::nouppercase << " - " << error_type << std::endl;
+
+	return EXCEPTION_CONTINUE_SEARCH;
+}
+
 int Thread()
 {
-	std::chrono::high_resolution_clock::time_point PrevTime, CurTime;
-	CurTime = std::chrono::high_resolution_clock::now();
-	while( true )
+	__try
 	{
-		PrevTime = CurTime;
+		std::chrono::high_resolution_clock::time_point PrevTime, CurTime;
 		CurTime = std::chrono::high_resolution_clock::now();
-		ElDorito::Instance().Tick(
-			std::chrono::duration_cast<std::chrono::duration<double>>(CurTime - PrevTime)
-			);
+		while(true)
+		{
+			PrevTime = CurTime;
+			CurTime = std::chrono::high_resolution_clock::now();
+			ElDorito::Instance().Tick(
+				std::chrono::duration_cast<std::chrono::duration<double>>(CurTime - PrevTime)
+				);
+		}
+	}
+	__except(TopLevelExceptionHandler(GetExceptionCode(), GetExceptionInformation())){
+		
 	}
 	return 0;
 }
