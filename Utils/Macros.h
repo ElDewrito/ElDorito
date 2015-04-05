26b5bbe1577ae822175c5bc2fe69fae76f0ae6e9
Wunkolo <wunkolo@gmail.com>
2015-04-05 09:43:31 -0700

    inlined util macros

--- Utils/Macros.h
+++ Utils/Macros.h
@@ -8,7 +8,7 @@ namespace Utils
 	// array[5]
 	// GetArraySize(array) = 5
 	template <typename T, size_t Num>
-	size_t GetArraySize(T(&a)[Num])
+	inline size_t GetArraySize(T(&a)[Num])
 	{
 		return Num
 	}
@@ -37,7 +37,7 @@ namespace Utils
 		return (n >= high) ? high : ((n <= low) ? low : n);
 	}
 
-	bool GetBit(size_t n, size_t bit)
+	inline bool GetBit(size_t n, size_t bit)
 	{
 		return (n & (1 << bit)) ? true : false;
 	}
