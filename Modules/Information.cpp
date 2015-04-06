f05cf3528ccc7c783e856a1eb267bd79676cceb7
Wunkolo <wunkolo@gmail.com>
2015-04-05 17:32:32 -0700

    Added session info to info module

--- Modules/Information.cpp
+++ Modules/Information.cpp
@@ -32,5 +32,23 @@ bool Information::Run(const std::vector<std::string>& Args)
 
 	std::string LocalSecureKey((char*)Pointer(0x50CCDB4 + 1));
 	std::cout << "Local Secure Key: " << (LocalSecureKey.empty() ? "(null)" : LocalSecureKey) << std::endl;
+
+	std::string Build((char*)Pointer(0x199C0F0));
+	std::cout << "Build: " << (Build.empty() ? "(null)" : Build) << std::endl;
+
+	std::string SystemID((char*)Pointer(0x199C130));
+	std::cout << "SystemID: " << (SystemID.empty() ? "(null)" : SystemID) << std::endl;
+
+	std::string SessionID((char*)Pointer(0x199C1D0));
+	std::cout << "SessionID: " << (SessionID.empty() ? "(null)" : SessionID) << std::endl;
+
+	std::string DevKitName((char*)Pointer(0x160C8C8));
+	std::string DevKitVersion((char*)Pointer(0x199C350));
+	std::cout << "SDK Info: " << (DevKitName.empty() ? "(null)" : DevKitName) << '|';
+	std::cout << (DevKitVersion.empty() ? "(null)" : DevKitVersion) << std::endl;
+
+	std::string FlashVersion((char*)Pointer(0x199C350));
+	std::cout << "Flash Version: " << (FlashVersion.empty() ? "(null)" : FlashVersion) << std::endl;
+
 	return true;
 }
