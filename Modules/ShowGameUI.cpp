3291fd7a9c12dbc5b32ad3b6c916510ac43f38dc
Wunkolo <wunkolo@gmail.com>
2015-04-08 19:21:18 -0700

    some notes and suggestions for show_ui

--- Modules/ShowGameUI.cpp
+++ Modules/ShowGameUI.cpp
@@ -18,14 +18,24 @@ ShowGameUI::~ShowGameUI()
 
 std::string ShowGameUI::Info(const std::string& Topic) const
 {
-	std::string Info = "Show Game UI dialogs/widgets"
+	std::string Info = "Show Game UI dialogs/widgets [ Experimental ]"
 		"\nUsage: show_ui (stringID)\n"
-		"stringID should be the stringID of the UI element you want to show, in hexadecimal.";
+		"stringID should be the stringID of the UI element you want to show, in hexadecimal.\n";
+	Info += "Some sample StringIDs:\n";
+	Info += "10083 main_menu\n";
+	Info += "10084 start_menu\n";
 	return Info;
 }
 
 std::string ShowGameUI::Suggest(const std::vector<std::string>& Arguments) const
 {
+	if( Arguments.size() == 2 )
+	{
+		if( Arguments[1].empty() )
+		{
+			return "10083";
+		}
+	}
 	return "";
 }
 
@@ -37,15 +47,15 @@ bool ShowGameUI::Run(const std::vector<std::string>& Args)
 {
 	static uint8_t UIData[0x40];
 
-	if (Args.size() < 2)
+	if( Args.size() < 2 )
 		return false;
 
 	int parentDialog = 0x1000D;
-	if (Args.size() >= 3)
+	if( Args.size() >= 3 )
 		parentDialog = std::stoul(Args[2], nullptr, 16);
 
 	int openType = 0;
-	if (Args.size() >= 4)
+	if( Args.size() >= 4 )
 		openType = std::stoul(Args[3], nullptr, 16);
 
 	unsigned int stringID = std::stoul(Args[1], nullptr, 16);
