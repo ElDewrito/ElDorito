5293dc698246300c1ff976d950fe67ca54835303
emoose <abc@cock.li>
2015-05-03 02:15:39 +0100

    add hostname support in connect command, fix port not being set

--- Modules/DirectConnect.cpp
+++ Modules/DirectConnect.cpp
@@ -1,8 +1,9 @@
-#include "DirectConnect.h"
-
-#include "../ElDorito.h"
-
+#include <WinSock2.h>
+#include <WS2tcpip.h>
 #include <Windows.h>
+
+#include "DirectConnect.h"
+#include "../ElDorito.h"
 #include <iostream>
 
 DirectConnect::DirectConnect()
@@ -54,18 +55,55 @@ bool DirectConnect::Run(const std::vector<std::string>& Args)
 		memset(rawIpaddr, 0, 4);
 
 		size_t portOffset = address.find(':');
-
-		uint32_t ipAddr[0x4];
-		sscanf_s(address.c_str(), "%d.%d.%d.%d", &ipAddr[3], &ipAddr[2], &ipAddr[1], &ipAddr[0]);
-		for (int i = 3; i >= 0; i--)
-			rawIpaddr[i] = (BYTE)ipAddr[i];
-
+		std::string host = address;
 		if (portOffset != std::string::npos && portOffset + 1 < address.size())
 		{
 			std::string port = address.substr(portOffset + 1);
 			rawPort = (uint16_t)atoi(port.c_str());
+			host = address.substr(0, portOffset);
 		}
 
+		struct addrinfo* info = NULL;
+		INT retval = getaddrinfo(host.c_str(), "11774", NULL, &info);
+		if (retval != 0)
+		{
+			int error = WSAGetLastError();
+			std::cout << "Unable to lookup " << address << " (" << retval << "): ";
+			if (error != 0)
+			{
+				if (error == WSAHOST_NOT_FOUND)
+					std::cout << "Host not found.\n" << std::endl;
+				else if (error == WSANO_DATA)
+					std::cout << "No data record found.\n" << std::endl;
+				else
+					std::cout << "Function failed with error " << error << ".\n" << std::endl;
+			}
+			else
+				std::cout << "Unknown error.\n" << std::endl;
+			return true;
+		}
+		struct addrinfo *ptr = NULL;
+		bool foundAddr = false;
+		for (ptr = info; ptr != NULL; ptr = ptr->ai_next)
+		{
+			if (ptr->ai_family != AF_INET)
+				continue; // not ipv4
+
+			BYTE flippedIpaddr[0x4];
+			memcpy(flippedIpaddr, &((sockaddr_in*)ptr->ai_addr)->sin_addr, 4);
+			rawIpaddr[3] = flippedIpaddr[0];
+			rawIpaddr[2] = flippedIpaddr[1];
+			rawIpaddr[1] = flippedIpaddr[2];
+			rawIpaddr[0] = flippedIpaddr[3];
+
+			foundAddr = true;
+			break;
+		}
+		if (!foundAddr)
+		{
+			std::cout << "Unable to lookup " << address << ": No records found.\n" << std::endl;
+			return true;
+		}
 		Utils::String::HexStringToBytes(xnkid, rawXnkid, 0x10);
 		Utils::String::HexStringToBytes(xnaddr, rawXnaddr, 0x10);
 
@@ -79,7 +117,7 @@ bool DirectConnect::Run(const std::vector<std::string>& Args)
 
 		memcpy(syslinkData + 0x170, rawIpaddr, 0x4);
 
-		*(uint16_t*)(syslinkData + 0x174) = 11774;
+		*(uint16_t*)(syslinkData + 0x174) = rawPort;
 
 		Pointer::Base(0x1E8E6D8).Write<uint32_t>(1);
 		Pointer::Base(0x1E8E6DC).Write<uint32_t>((uint32_t)&syslinkData);
