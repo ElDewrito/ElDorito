264df7976ca6996eecc383c0fffd0b19bac4490b
null <null>
2015-06-18 09:33:11 -0400

    Applied emoose's patch to remove boost dependencies

--- ThirdParty/MedalJunk/DLL/OpticPack.cpp
+++ ThirdParty/MedalJunk/DLL/OpticPack.cpp
@@ -5,7 +5,10 @@
 #include <memory>
 #include <iostream>
 #include <Windows.h>
-#include <boost\algorithm\string.hpp>
+#include <algorithm> 
+#include <functional> 
+#include <cctype>
+#include <locale>
 
 namespace Optic {
 
@@ -125,6 +128,23 @@ std::string OpticPack::getLua() const {
 	}
 }
 
+// trim from start
+static inline std::string &ltrim(std::string &s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+	return s;
+}
+
+// trim from end
+static inline std::string &rtrim(std::string &s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	return s;
+}
+
+// trim from both ends
+static inline std::string &trim(std::string &s) {
+	return ltrim(rtrim(s));
+}
+
 std::map<std::string, std::string> OpticPack::parseMeta(std::unique_ptr<char[]> buffer) {
 	std::map<std::string, std::string> meta;
 	std::istringstream ifs(buffer.get());
@@ -135,8 +155,8 @@ std::map<std::string, std::string> OpticPack::parseMeta(std::unique_ptr<char[]> 
 		if(pos != std::string::npos) {
 			std::string key = line.substr(0, pos);
 			std::string value = line.substr(pos + 1);
-			boost::trim(key);
-			boost::trim(value);
+			key = trim(key);
+			value = trim(value);
 			meta[key] = value;
 		}
 	}
