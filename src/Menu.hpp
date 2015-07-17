53795fafe70210e62bbf664d3d403e3b9867f249
ImplodingExplosion <coolman3428@gmail.com>
2015-07-17 05:17:01 -0500

    Work with overlaying menu

--- src/Menu.hpp
+++ src/Menu.hpp
@@ -11,20 +11,19 @@ class Menu : public Utils::Singleton<Menu>
 {
 private:
 	int pid = 0;
-	HWND hWnd = *((HWND*)0x199C014);
 	static char* mapLoading;
 	static int(__cdecl * loadFinished)(void*);
 	static char(__cdecl * loadMap)(void*);
 
 	bool doesFileExist(const char *fileName);
 	bool doesDirExist(const std::string& dirName_in);
-	bool isMenuRunning();
 	bool isRunning(int pid);
 	static int __cdecl hookedLoadFinished(void* a1);
 	static char __cdecl hookedLoadMap(void* a1);
 
 public:
 	void setEnabled(bool enable);
+	bool isMenuRunning();
 
 	Menu();
 };
