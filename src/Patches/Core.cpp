34503e3b6805b70c23d49877078398c1ec5af4f9
Shockfire <shock@420blaze.it>
2016-03-18 21:12:14 -0500

    Revert "Added active camo patch from @Lord-Zedd"
    This reverts commit 4f5c0b632543b1cf099ab64dbdb98a1ed7ba176e.
    
    Sorry Zedd, but this causes problems

--- src/Patches/Core.cpp
+++ src/Patches/Core.cpp
@@ -57,10 +57,7 @@ namespace Patches
 
 			// Fix random colored lighting
 			Patch(0x14F2FFC, { 0x0, 0x0, 0x0, 0x0 }).Apply();
-			
-			// Active camo fix
-			Patch(0x625ACA, { 0x20 }).Apply();
-			
+
 			// Hook game ticks
 			Hook(0x105E64, GameTickHook, HookFlags::IsCall).Apply();
 
