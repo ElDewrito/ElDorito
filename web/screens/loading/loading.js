099a6d5c782f43a41ceabf9ddb9e85ae2401c055
AssaultCommand <caspar.neervoort@gmail.com>
2016-03-29 11:47:41 +0200

    Reworked the loader structure, css and js a bit
    It should no longer display previous game information in the split
    second it takes to load the current game's info in.

--- web/screens/loading/loading.js
+++ web/screens/loading/loading.js
@@ -1,68 +1,6 @@
 var mapName = "";
 var gameModes = ["slayer","ctf","slayer","oddball","koth","forge","vip","juggernaut","territories","assault","infection"];
 
-function updateProgress(progress) {
-    $("#progressbar").attr('value', progress);
-    progress = 100 - progress;
-    $(".loading").css({"-webkit-clip-path": "inset(" + progress + "% 0 0 0)"});
-}
-
-dew.on("show", function (event) {
-    mapName = event.data.map || "";
-    if (mapName != "mainmenu") {
-        $(".generic").hide();
-        $(".container").show();
-        $("body").css({backgroundImage: "url('images/maps/" + mapName + ".png')"});
-        dew.getMapVariantInfo().then(function (info) {
-            $("#title").text(info.name);
-            $("#desc").text(info.description);
-        });
-        dew.getGameVariantInfo().then(function (info) {
-            $("#gametypeicon").attr("src", "images/gametypes/" + gameModes[info.mode] + ".png");
-            $("#gametype").text(info.name);  
-            if(info.rounds > 0){
-                $("#gamerounds").text(info.rounds);  
-            } else {
-                $("#gamerounds").text("Unlimited"); 
-            }
-            if(info.scoreToWin > -1){
-                $("#gamescore").text(info.scoreToWin);   
-            } else {
-                $("#gamescore").text("Unlimited");  
-            }
-            if(info.timeLimit > 0){
-                $("#timelimit").text(timeConvert(info.timeLimit)); 
-            } else {
-                $("#timelimit").text("Unlimited");  
-            }                
-        });
-        dew.command("Server.NameClient", { internal: true }).then(function (name) {
-            $(".serverName").text(name);
-        });
-        dew.command("Server.MessageClient", { internal: true }).then(function (message) {
-            if(message.length > 0){
-                $(".serverMessage").show();
-                $(".serverMessage").html(textWithNewLines(message));
-            } else {
-                $(".serverMessage").hide();
-            }
-        });
-        $(".header").show();
-        $(".footer").show();
-    } else {
-        $(".generic").show();
-        $(".header").hide();
-        $(".container").hide();
-        $(".footer").hide();
-    }
-    updateProgress(0);
-});
-
-dew.on("loadprogress", function (event) {
-    var progress = event.data.currentBytes / event.data.totalBytes * 100;
-    updateProgress(progress);
-});
-
 function textWithNewLines(text) {
     var htmls = [];
     var lines = text.split("\\n");
@@ -79,3 +17,80 @@ function timeConvert(mins){
     if (minutes <= 9) { minutes = "0" + minutes; };
     return hours +":"+ minutes;
 }
+
+function updateProgress(progress) {
+    $("#progressbar").attr('value', progress);
+    progress = 100 - progress;
+    $(".loading").css({"-webkit-clip-path": "inset(" + progress + "% 0 0 0)"});
+}
+
+function loadMap(mapName) {
+    $(".mapLoader").show();
+    $(".mapLoader").css({backgroundImage: "url('images/maps/" + mapName + ".png')"});
+    $(".genericLoader").hide();
+    dew.getMapVariantInfo().then(function (info) {
+        $("#title").text(info.name);
+        $("#desc").text(info.description);
+    });
+    dew.getGameVariantInfo().then(function (info) {
+        $("#gametypeicon").attr("src", "images/gametypes/" + gameModes[info.mode] + ".png");
+        $("#gametype").text(info.name);  
+        if(info.rounds > 0){
+            $("#gamerounds").text(info.rounds);  
+        } else {
+            $("#gamerounds").text("Unlimited"); 
+        }
+        if(info.scoreToWin > -1){
+            $("#gamescore").text(info.scoreToWin);   
+        } else {
+            $("#gamescore").text("Unlimited");  
+        }
+        if(info.timeLimit > 0){
+            $("#timelimit").text(timeConvert(info.timeLimit)); 
+        } else {
+            $("#timelimit").text("Unlimited");  
+        }                
+    });
+    dew.command("Server.NameClient", { internal: true }).then(function (name) {
+        $(".serverName").text(name);
+    });
+    dew.command("Server.MessageClient", { internal: true }).then(function (message) {
+        if(message.length > 0){
+            $(".serverMessage").show();
+            $(".serverMessage").html(textWithNewLines(message));
+        } else {
+            $(".serverMessage").hide();
+        }
+    });
+}
+
+function loadGeneric() {
+    $(".genericLoader").show();
+    $(".mapLoader").hide();
+}
+
+function reseetLoader() {
+    $(".genericLoaderm, .mapLoader").hide();
+    $("#gametype, #gamerounds, #gamescore, #timelimit, .serverName, .serverMessage, .title, .desc").text("");
+    $(".mapLoader").css({backgroundImage: ""});
+    $("#gametypeicon").attr("src", "");
+}
+
+dew.on("show", function (event) {
+    mapName = event.data.map || "";
+    if (mapName != "mainmenu") {
+        loadMap(mapName);
+    } else {
+        loadGeneric();
+    }
+    updateProgress(0);
+});
+
+dew.on("hide", function (e) {
+    resetLoader();
+});
+
+dew.on("loadprogress", function (event) {
+    var progress = event.data.currentBytes / event.data.totalBytes * 100;
+    updateProgress(progress);
+});
