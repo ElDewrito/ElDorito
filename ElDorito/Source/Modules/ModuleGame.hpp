19ad8bf30219bdbf0142f275cfae0b52a08be4d7
theTwist84 <lee.foulger.94@gmail.com>
2017-05-02 22:22:26 +0100

    Add PancamEnabled command to show player coordinates

--- ElDorito/Source/Modules/ModuleGame.hpp
+++ ElDorito/Source/Modules/ModuleGame.hpp
@@ -14,6 +14,7 @@ namespace Modules
 		Command* VarRconPort;
 		Command* VarMedalPack;
 		Command* VarFirstRun;
+		Command* VarPancamEnabled;
 
 		int DebugFlags;
 
---
+++ ElDorito/Source/Patches/Pancam.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Patches
+{
+	namespace Pancam
+	{
+		void Enable(bool enabled);
+	}
+}
