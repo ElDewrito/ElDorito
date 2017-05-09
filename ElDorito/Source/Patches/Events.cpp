eb6bb9f3ca11d8130f9a0c8ebdd8151bddbfb901
RabidSquabbit <rabidsquabbit@gmail.com>
2017-05-08 21:34:30 -0500

    Fix for Oddball and Koth scoreboard not updating

--- ElDorito/Source/Patches/Events.cpp
+++ ElDorito/Source/Patches/Events.cpp
@@ -1,6 +1,9 @@
 #include "Events.hpp"
 #include "../Blam/BlamEvents.hpp"
 #include "../Patch.hpp"
+#include "../Web/Ui/WebScoreboard.hpp"
+#include "../Web/Ui/ScreenLayer.hpp"
+
 #include <vector>
 
 using namespace Blam::Events;
@@ -9,8 +12,10 @@ using namespace Patches::Events;
 namespace
 {
 	void RunEventHook(Blam::DatumIndex player, const Event *event, const EventDefinition *definition);
-
+	void __cdecl UpdateScoreboardEventHookHost(int a1, unsigned int a2, int a3, int a4);
+	char __cdecl UpdateScoreboardEventHook(int a1, int a2, int a3);
 	std::vector<EventCallback> OnEventCallbacks;
+
 }
 
 namespace Patches
@@ -22,6 +27,9 @@ namespace Patches
 			Hook(0x16669C, RunEventHook, HookFlags::IsCall).Apply();
 			Hook(0x165CE2, RunEventHook, HookFlags::IsCall).Apply();
 			Hook(0x165D0D, RunEventHook, HookFlags::IsCall).Apply();
+			Hook(0x2E5A24, UpdateScoreboardEventHookHost, HookFlags::IsCall).Apply();
+			Hook(0xC654D, UpdateScoreboardEventHook, HookFlags::IsCall).Apply();
+
 		}
 
 		void OnEvent(EventCallback callback)
@@ -33,6 +41,25 @@ namespace Patches
 
 namespace
 {
+	void __cdecl UpdateScoreboardEventHookHost(int a1, unsigned int a2,  int a3, int a4)
+	{
+		Web::Ui::ScreenLayer::Notify("scoreboard", Web::Ui::WebScoreboard::getScoreboard(), true);
+
+		typedef char(*UpdateScoreboard)(int a1, unsigned int a2, int a3, int a4);
+		UpdateScoreboard Update = reinterpret_cast<UpdateScoreboard>(0x5704A0);
+		Update(a1, a2, a3, a4);
+
+	}
+	char __cdecl UpdateScoreboardEventHook(int a1, int a2, int a3)
+	{
+		Web::Ui::ScreenLayer::Notify("scoreboard", Web::Ui::WebScoreboard::getScoreboard(), true);
+
+		typedef char(*UpdateScoreboard)(int a1, int a2, int a3);
+		UpdateScoreboard Update = reinterpret_cast<UpdateScoreboard>(0x566070);
+		return Update(a1, a2, a3);
+
+	}
+
 	void RunEventHook(Blam::DatumIndex player, const Event *event, const EventDefinition *definition)
 	{
 		// Dispatch the event to handlers
