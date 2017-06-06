002b5a627891f246ef42611e8c8bf6aae022ee41
scooterpsu <scooterpsu@gmail.com>
2017-06-06 18:55:56 -0400

    Forge UI - Controller support, styling changes from Settings

--- dist/mods/ui/web/screens/forge/forge.js
+++ dist/mods/ui/web/screens/forge/forge.js
@@ -1,5 +1,9 @@
 var settingsToLoad = [['fCloneDepth', 'Forge.CloneDepth'], ['fCloneMultiplier', 'Forge.CloneMultiplier'], ['fRotationSnap', 'Forge.RotationSnap'], ['fRotationSens', 'Forge.RotationSensitivity']];
 
+var selectedItem;
+var itemNumber = 0;
+var tabIndex = 0;
+
 $(document).keyup(function (e) {
     if (e.keyCode === 27) {
         $('#playerBreakdown').hide();
@@ -25,7 +29,10 @@ $(document).ready(function() {
     loadSettings(0);     
     $('#forgeControls input').on('change', function(){
         updateSetting(this.name, this.value);
-    });    
+    }); 
+    if(hasGP){
+        updateSelection(itemNumber);
+    }    
 });
 
 function loadSettings(i) {
@@ -69,3 +76,128 @@ function arrayInArray(needle, haystack) {
         }
     }
 }
+
+function updateSelection(item){
+    $('.selected').removeClass();
+    $("#forgeControls label").eq(item).addClass('selected');
+    selectedItem = $('#forgeControls').children().not('label,center,br,.tinySetting,.header,p').eq(itemNumber).attr('id');
+}
+
+function upNav(){
+    if(itemNumber > 0){
+        itemNumber--;
+        updateSelection(itemNumber);
+    }
+}
+
+function downNav(){
+    if(itemNumber < $('#forgeControls label').length-1){
+        itemNumber++;
+        updateSelection(itemNumber);
+    }           
+}
+
+function leftToggle(){
+    if($('#'+selectedItem).getInputType() == "select"){
+        var elementIndex = $('#'+selectedItem+' option:selected').index();
+        if(elementIndex > 0){
+            var newElement = elementIndex - 1;
+            $('#'+selectedItem+' option').eq(newElement).prop('selected', true);
+            if(selectedItem == 'presetMenu'){
+                applyBindString($('#'+selectedItem).val());
+            } else {
+                updateSetting($('#'+selectedItem).attr('name'), $('#'+selectedItem).val());
+            }
+        }
+    }
+    if($('#'+selectedItem).getInputType() == "range"){
+        document.getElementById(selectedItem).stepDown();
+        document.querySelector('#'+selectedItem +'Text').value = document.getElementById(selectedItem).value;
+        updateSetting($('#'+selectedItem).attr('name'), $('#'+selectedItem).val());
+    }        
+}
+
+function rightToggle(){
+    if($('#'+selectedItem).getInputType() == "select"){
+        var elementIndex = $('#'+selectedItem+' option:selected').index();
+        var elementLength = $('#'+selectedItem).children('option').length;
+        if(elementIndex < elementLength){
+            var newElement = elementIndex + 1;
+            $('#'+selectedItem+' option').eq(newElement).prop('selected', true);
+            if(selectedItem == 'presetMenu'){
+                applyBindString($('#'+selectedItem).val());
+            } else {
+                updateSetting($('#'+selectedItem).attr('name'), $('#'+selectedItem).val());
+            }
+        }
+    }
+    if($('#'+selectedItem).getInputType() == "range"){
+        document.getElementById(selectedItem).stepUp();
+        document.querySelector('#'+selectedItem +'Text').value = document.getElementById(selectedItem).value;
+        updateSetting($('#'+selectedItem).attr('name'), $('#'+selectedItem).val());
+    }        
+}
+
+$.fn.getInputType = function () {
+    return this[0].tagName.toString().toLowerCase() === "input" ?
+        $(this[0]).attr("type").toLowerCase() : this[0].tagName.toLowerCase();
+};
+
+function onControllerConnect(){
+    updateSelection(itemNumber); 
+    $('button img').show();
+}
+
+function onControllerDisconnect(){
+    $('.selected').removeClass(); 
+    $('button img').hide();    
+}
+
+function buttonAction(i){
+    switch (i) {
+        case 1: // B
+            $('#playerBreakdown').hide();
+            dew.hide();
+            break;
+        case 2: // X
+            dew.command('Forge.Canvas');
+            break;
+        case 3: // Y
+            dew.command('Forge.DeleteAll');
+            break;
+        case 12: // Up
+            upNav();
+            break;
+        case 13: // Down
+            downNav();
+            break;
+        case 14: // Left
+            leftToggle();
+            break;
+        case 15: // Right
+            rightToggle();
+            break;
+        case 4: // LB
+            prevPage();
+            break;
+        case 5: // RB
+            nextPage();
+            break;
+        default:
+            //console.log("nothing associated with " + i);
+    }  
+}
+
+function stickAction(direction, x){
+    if(x<2){//left stick
+        if(x==0 && direction=="+"){//LS Right
+            rightToggle();
+        }else if(x==0 && direction=="-"){//LS Left
+            leftToggle();
+        }else if(x==1 && direction=="+"){//LS Down
+            downNav();
+        }else if(x==1 && direction=="-"){//LS Up
+            upNav();
+        }
+    }
+};
