4f7d9b0f183d366da9e2bdf3b98b43ecbc8da288
scooterpsu <scooterpsu@gmail.com>
2017-07-05 17:35:00 -0400

    Chat - An _actual_ fix for an XSS vector

--- dist/mods/ui/web/screens/chat/chat.js
+++ dist/mods/ui/web/screens/chat/chat.js
@@ -143,7 +143,8 @@ $(window).load(function(){
                 chatClass += ' emote';
                 e.data.message = e.data.message.substring(4, e.data.message.length);
             }
-            $("#chatWindow").append($('<span>', { class: messageClass, css: { backgroundColor: bgColor}, text: e.data.sender }).wrap($('<p>', { class: chatClass })).parent().append($("<div>").text(e.data.message).text().replace(/\bhttp[^ ]+/ig, aWrap))); 
+            var messageText = e.data.message.replace(/\</g,"&lt;").replace(/\>/g,"&gt;");
+            $("#chatWindow").append($('<span>', { class: messageClass, css: { backgroundColor: bgColor}, text: e.data.sender }).wrap($('<p>', { class: chatClass })).parent().append($("<div>").text(messageText).text().replace(/\bhttp[^ ]+/ig, aWrap))); 
             if(!hideChat){
                 dew.show();
             }
