c89a4b7dc8d9167e9b11e0208f92dac872a98e38
Alex231 <a13x231mcsl@gmail.com>
2017-08-13 17:55:34 +0100

    Added bottomless clip symbols.

--- ElDorito/Source/Patches/BottomlessClip.cpp
+++ ElDorito/Source/Patches/BottomlessClip.cpp
@@ -5,10 +5,16 @@
 
 namespace
 {
+	void AmmoCountInputHook();
+
 	bool bottomlessClipEnabled = true;
-	//nop ammo depletion.
+
 	Patch enableBottomlessClip = Patch(0x75FACD, 0x90, 8);
 	Patch enableBottomlessBattery = Patch(0x75BE42, 0x90, 8); //Weapons with battery, like the plasma pistol.
+
+	Hook totalAmmoInfiniteSymbol = Hook(0x68B6DF, AmmoCountInputHook, HookFlags::IsCall);
+	Hook clipAmmoInfiniteSymbol = Hook(0x68B6C2, AmmoCountInputHook, HookFlags::IsCall);
+	Hook batteryInfiniteSymbol = Hook(0x68B72D, AmmoCountInputHook, HookFlags::IsCall);
 }
 
 namespace Patches::BottomlessClip
@@ -18,13 +24,36 @@ namespace Patches::BottomlessClip
 		bottomlessClipEnabled = enabled;
 		if (enabled)
 		{
+			//Enable bottomless clip.
 			enableBottomlessClip.Apply();
 			enableBottomlessBattery.Apply();
+			//Enabled infinity symbols.
+			totalAmmoInfiniteSymbol.Apply();
+			clipAmmoInfiniteSymbol.Apply();
+			batteryInfiniteSymbol.Apply();
 		}
 		else
 		{
+			//Disable bottomless clip.
 			enableBottomlessClip.Reset();
 			enableBottomlessBattery.Reset();
+			//Disable infinity symbols.
+			totalAmmoInfiniteSymbol.Reset();
+			clipAmmoInfiniteSymbol.Reset();
+			batteryInfiniteSymbol.Reset();
+		}
+	}
+}
+
+namespace 
+{
+	__declspec(naked) void AmmoCountInputHook()
+	{
+		__asm
+		{
+			mov ecx, 0x7F7FFFFF
+			movd xmm0, ecx
+			ret
 		}
 	}
 }
