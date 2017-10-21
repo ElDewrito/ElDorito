e28b0f25717e9a86b7e32c3a1b57f730edba9a26
maximumgame <nickemerich@gmail.com>
2017-10-21 16:31:11 -0400

    Use action states instead of xinput

--- ElDorito/Source/Web/Ui/WebSettings.cpp
+++ ElDorito/Source/Web/Ui/WebSettings.cpp
@@ -1,11 +1,8 @@
 #include "ScreenLayer.hpp"
 #include "../../Patches/Input.hpp"
-#include "../../Pointer.hpp"
 
-#include "Xinput.h"
 #include "../../Modules/ModuleInput.hpp"
 #include "../../Modules/ModuleSettings.hpp"
-#include <boost/thread/futures/future_status.hpp>
 
 namespace
 {
@@ -31,16 +28,11 @@ namespace
 	{
 		if (Modules::ModuleSettings::Instance().VarGamepadEnabled->ValueInt == 1)
 		{
-			// Due to start being bound to escape on the keyboard we can't use action states.
-			XINPUT_STATE state;
-			XInputGetState(0, &state);
-			if((state.Gamepad.wButtons & XINPUT_GAMEPAD_START) == XINPUT_GAMEPAD_START && strcmp((char*)Pointer(0x22AB018)(0x1A4), "mainmenu") == 0)
+			if(Blam::Input::GetActionState(Blam::Input::eGameActionUiStart)->Ticks == 1 && strcmp((char*)Pointer(0x22AB018)(0x1A4), "mainmenu") == 0)
 				Web::Ui::WebSettings::Show();
 		}
-		else
-		{
-			if(GetKeyTicks(Blam::Input::eKeyCodeHome, Blam::Input::eInputTypeUi) && strcmp((char*)Pointer(0x22AB018)(0x1A4), "mainmenu") == 0)
-				Web::Ui::WebSettings::Show();
-		}
+		
+		if(GetKeyTicks(Blam::Input::eKeyCodeHome, Blam::Input::eInputTypeUi) && strcmp((char*)Pointer(0x22AB018)(0x1A4), "mainmenu") == 0)
+			Web::Ui::WebSettings::Show();
 	}
 }
