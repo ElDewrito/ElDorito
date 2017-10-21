c6c5630ccc7de5482caea0f9cc6d5b4140633e21
maximumgame <nickemerich@gmail.com>
2017-10-21 12:21:23 -0400

    Only check xinput if gamepad is enabled
    Fixes #340

--- ElDorito/Source/Web/Ui/WebSettings.cpp
+++ ElDorito/Source/Web/Ui/WebSettings.cpp
@@ -4,6 +4,7 @@
 
 #include "Xinput.h"
 #include "../../Modules/ModuleInput.hpp"
+#include "../../Modules/ModuleSettings.hpp"
 #include <boost/thread/futures/future_status.hpp>
 
 namespace
@@ -28,11 +29,18 @@ namespace
 {
 	void OnGameInputUpdated()
 	{
-		// Due to start being bound to escape on the keyboard we can't use action states.
-		XINPUT_STATE state;
-		XInputGetState(0, &state);
-
-		if (((state.Gamepad.wButtons & XINPUT_GAMEPAD_START) != 0 || GetKeyTicks(Blam::Input::eKeyCodeHome, Blam::Input::eInputTypeUi)) && strcmp((char*)Pointer(0x22AB018)(0x1A4), "mainmenu") == 0)
-			Web::Ui::WebSettings::Show();
+		if (Modules::ModuleSettings::Instance().VarGamepadEnabled->ValueInt == 1)
+		{
+			// Due to start being bound to escape on the keyboard we can't use action states.
+			XINPUT_STATE state;
+			XInputGetState(0, &state);
+			if((state.Gamepad.wButtons & XINPUT_GAMEPAD_START) == XINPUT_GAMEPAD_START && strcmp((char*)Pointer(0x22AB018)(0x1A4), "mainmenu") == 0)
+				Web::Ui::WebSettings::Show();
+		}
+		else
+		{
+			if(GetKeyTicks(Blam::Input::eKeyCodeHome, Blam::Input::eInputTypeUi) && strcmp((char*)Pointer(0x22AB018)(0x1A4), "mainmenu") == 0)
+				Web::Ui::WebSettings::Show();
+		}
 	}
 }
