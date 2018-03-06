9d71454810b2b24d7c3b5017e3925288130e5876
unk-1 <unk0x4@gmail.com>
2018-03-06 21:20:02 +0000

    BlamData optimization

--- ElDorito/Source/Blam/BlamData.cpp
+++ ElDorito/Source/Blam/BlamData.cpp
@@ -2,49 +2,72 @@
 
 namespace Blam
 {
-
 	DatumBase* DataArrayBase::Get(DatumHandle index) const
 	{
-		typedef DatumBase*(*DataArrayGetPtr)(const DataArrayBase *data, DatumHandle index);
-		auto DataArrayGet = reinterpret_cast<DataArrayGetPtr>(0x55B6D0);
-		return DataArrayGet(this, index);
+		if (index.Index == 0xffff || index.Index >= this->FirstUnallocated)
+			return nullptr;
+
+		auto datum = this->GetAddress(index);
+		auto salt = datum->GetSalt();
+		if (!salt || salt != index.Salt)
+			return nullptr;
+
+		return datum;
 	}
 
 	void DataArrayBase::Delete(DatumHandle index)
 	{
-		const auto datum_array_free_datum = (void(*)(void *a1, unsigned __int16 datumIndex))(0x0055B2E0);
-		datum_array_free_datum(this, index.Index);
-		return;
-
-		if (index.Index == 0xffff)
-			return;
-
-		auto datum = (Blam::DatumBase*)((uint8_t*)this->Data + index.Index * this->DatumSize);
-		if ((this->Flags >> 3) & 1)
+		uint8_t *datum;
+		datum = ((uint8_t*)this->Data + index.Index * this->DatumSize);
+		if (((this->Flags & 0xff) >> 3) & 1)
 			memset(datum, 0xBA, this->DatumSize);
-		this->ActiveIndices[index.Index >> 5] &= ~(1 << index.Index & 0x1F);
-		*(uint16_t *)datum = 0;
+		this->ActiveIndices[index.Index >> 5] &= ~(1 << (index.Index & 0x1F));
+		*datum = 0;
 		if (index.Index < this->NextIndex)
-			this->NextIndex = index.Index;
-		auto nextIndex = index.Index + 1;
-		if (nextIndex == this->FirstUnallocated)
+			this->NextIndex = index;
+		if ((index.Index + 1) == this->FirstUnallocated)
 		{
-			auto p = (uint8_t*)datum;
 			do
 			{
 				--this->FirstUnallocated;
-				p -= this->DatumSize;	
-			} 
-			while (this->FirstUnallocated > 0 && !*(uint16_t *)datum);
+				datum -= this->DatumSize;
+			} while (this->FirstUnallocated > 0 && !*datum);
 		}
 		--this->ActualCount;
 	}
 
+	uint32_t DataArray_GetNextIndex(const DataArrayBase *data, int index)
+	{
+		auto result = index;
+		if (index < 0 || index >= data->FirstUnallocated)
+			return -1;
+
+		while (!((1 << (result & 0x1F)) & data->ActiveIndices[result >> 5]))
+		{
+			if (++result >= data->FirstUnallocated)
+				return -1;
+		}
+
+		return result;
+	}
+
 	DatumBase* DataIteratorBase::Next()
 	{
-		typedef DatumBase*(*DataIteratorNextPtr)(DataIteratorBase *iterator);
-		auto DataIteratorNext = reinterpret_cast<DataIteratorNextPtr>(0x55AE30);
-		return DataIteratorNext(this);
+		auto index = DataArray_GetNextIndex(this->Array, this->CurrentIndex + 1);
+
+		if (index == -1)
+		{
+			this->CurrentIndex = this->Array->MaxCount;
+			this->CurrentDatumIndex = -1;
+			return nullptr;
+		}
+		else
+		{
+			auto datum = (Blam::DatumBase*)((uint8_t*)this->Array->Data + index * this->Array->DatumSize);
+			this->CurrentIndex = index;
+			this->CurrentDatumIndex = Blam::DatumHandle(index, datum->GetSalt());
+			return datum;
+		}		
 	}
 
 	int CalculateDatumArraySize(int datumCount, int datumSize, int alignmentBits)
