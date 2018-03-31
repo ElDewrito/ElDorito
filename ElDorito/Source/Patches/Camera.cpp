33799f37f3e493d31532fe182d505a519680e06b
unk-1 <unk0x4@gmail.com>
2018-03-31 11:27:00 +0100

    Player scaling - scale third person camera distance

--- ElDorito/Source/Patches/Camera.cpp
+++ ElDorito/Source/Patches/Camera.cpp
@@ -1,12 +1,13 @@
 #include "Camera.hpp"
+#include "../Blam/BlamObjects.hpp"
 #include "../Modules/ModuleCamera.hpp"
 #include <cstdint>
 
 namespace
 {
 	float GetScriptedCameraFovHook();
-	float ThirdPersonFovHook(uint32_t unitObjectIndex, float fov, int scopeLevel);
 	void DeadCameraFovHook();
+	void __fastcall c_following_camera__update_hook(void *ptr, void *unused, int localPlayerIndex, int a4, uint8_t *state);
 }
 
 namespace Patches::Camera
@@ -14,8 +15,8 @@ namespace Patches::Camera
 	void ApplyAll()
 	{
 		Hook(0x32E18C, GetScriptedCameraFovHook, HookFlags::IsCall).Apply();
-		Hook(0x32990C, ThirdPersonFovHook, HookFlags::IsCall).Apply();
 		Hook(0x2122C5, DeadCameraFovHook).Apply();
+		Pointer(0x016724DC).Write(uint32_t(&c_following_camera__update_hook));
 	}
 
 	void IncreaseLOD()
@@ -52,13 +53,6 @@ namespace
 		return fov * 0.017453292f;
 	}
 
-	float ThirdPersonFovHook(uint32_t unitObjectIndex, float fov, int scopeLevel)
-	{
-		const auto sub_B44080 = (float(*)(uint32_t unitObjectIndex, float fov, int scopeLevel))(0xB44080);
-		const auto &moduleCamera = Modules::ModuleCamera::Instance();
-		return sub_B44080(unitObjectIndex, fov / moduleCamera.VarCameraFov->DefaultValueFloat * moduleCamera.VarCameraFov->ValueFloat, scopeLevel);
-	}
-
 	float *GetPreferedCameraFovPointer()
 	{
 		return &Modules::ModuleCamera::Instance().VarCameraFov->ValueFloat;
@@ -80,5 +74,41 @@ namespace
 			mov eax, 0x0061230A
 			jmp eax
 		}
+	}
+
+	void __fastcall c_following_camera__update_hook(void *thisptr, void *unused, int localPlayerIndex, int a4, uint8_t *state)
+	{
+		struct s_camera_definition
+		{
+			Blam::Math::RealVector3D Position;
+			Blam::Math::RealVector3D PositionShift;
+			float LookShift[2];
+			float Depth;
+			float FieldOfView;
+			Blam::Math::RealVector3D Forward;
+			Blam::Math::RealVector3D Up;
+			// ...
+		};
+
+		const auto c_following_camera__update = (void(__thiscall *)(void *ptr, int localPlayerIndex, int a4, uint8_t *state))(0x00729730);
+		const auto &moduleCamera = Modules::ModuleCamera::Instance();
+
+		c_following_camera__update(thisptr, localPlayerIndex, a4, state);
+
+		auto &definition = *(s_camera_definition*)(state + 0x4);
+
+		// scale fov by prefered fov
+		definition.FieldOfView = definition.FieldOfView / moduleCamera.VarCameraFov->DefaultValueFloat * moduleCamera.VarCameraFov->ValueFloat;
+
+		// scale position shift and depth with target object scale
+		auto targetObjectIndex = *(uint32_t*)((uint8_t*)thisptr + 0x4);
+		if (targetObjectIndex == -1)
+			return;
+		auto targetObject = Blam::Objects::Get(targetObjectIndex);
+		if (!targetObject)
+			return;	
+		auto s = std::pow(targetObject->Scale, 0.5);
+		definition.Depth *= s;
+		definition.PositionShift *= s;
 	}
 }
